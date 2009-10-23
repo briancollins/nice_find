bf401313274156ec1623a8cc58bbe5b3f90a0971
Brian Collins <brian@livecurrent.com>
2009-10-23 14:45:15 -0700

    stack trace, overriding the action causer

--- NiceFind.mm
+++ NiceFind.mm
@@ -9,6 +9,7 @@
 #import "NiceFind.h"
 #import "MethodSwizzle.h"
 #import <objc/runtime.h>
+#import "GTMStackTrace.h"
 
 @implementation NiceFind
 
@@ -17,6 +18,7 @@
 	self = [self init];
 	NSApp = [NSApplication sharedApplication];
 	MethodSwizzle(objc_getClass("OakProjectController"), @selector(findInProjectWithOptions:), @selector(fip:));
+	MethodSwizzle(objc_getClass("OakFindManager"), @selector(performFindInProjectAction:), @selector(performFindInProjectActionNew:));
 	return self;
 }
 
@@ -34,9 +36,24 @@
 
 
 - (id)fip:(id)fp8 {
+	NSLog(@"%@", GTMStackTrace());
 	NSLog(@"input: %@", fp8);
 	id result = [self fip:fp8];
 	NSLog(@"result: %@", result);
 	return result;
 }
 @end 
+
+@interface OakFindManager : NSObject 
+@end
+
+@interface OakFindManager (nice_find)
+@end
+
+@implementation OakFindManager (nice_find)
+
+- (void)performFindInProjectActionNew:(id)obj {
+
+	[self performFindInProjectActionNew: obj];
+}
+@end
